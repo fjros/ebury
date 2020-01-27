43d12a1f65e5fd9306dd2893cc5b36255572260b
Francisco Ros <fjrosmunoz@gmail.com>
2020-01-27 19:17:50 +0100

    Frontend: Fix how TradeTable shows amounts (units instead of cents of units)
    This commit also fixes the indentation of TradeTable.vue

--- client/src/components/TradeTable.vue
+++ client/src/components/TradeTable.vue
@@ -1,71 +1,74 @@
 <template>
-    <div id="trade-table">
-        <h1>Booked Trades</h1>
-        <thead>
-            <tr>
-                <th>Sell CCY</th>
-                <th>Sell Amount</th>
-                <th>Buy CCY</th>
-                <th>Buy Amount</th>
-                <th>Rate</th>
-                <th>Date Booked</th>
-            </tr>
-        </thead>
+  <div id="trade-table">
+    <h1>Booked Trades</h1>
+    <thead>
+      <tr>
+        <th>Sell CCY</th>
+        <th>Sell Amount</th>
+        <th>Buy CCY</th>
+        <th>Buy Amount</th>
+        <th>Rate</th>
+        <th>Date Booked</th>
+      </tr>
+    </thead>
 
-        <template v-if="hasTrades">  <!-- show trades -->
-            <tbody>
-            <tr v-for="trade in trades" :key="trade.id">
-                    <td>{{ trade.sell_currency }}</td>
-                    <td>{{ trade.sell_amount }}</td>
-                    <td>{{ trade.buy_currency }}</td>
-                    <td>{{ trade.buy_amount }}</td>
-                    <td>{{ trade.rate }}</td>
-                    <td>{{ trade.created_at }}</td>
-                </tr>
-            </tbody>
-        </template>
-        
-        <template v-else>            <!-- show message if no trades yet -->
-            <p>No trades booked yet</p>
-        </template>
-    </div>
+    <template v-if="hasTrades">
+      <!-- show trades -->
+      <tbody>
+        <tr v-for="trade in trades" :key="trade.id">
+          <td>{{ trade.sell_currency }}</td>
+          <td>{{ trade.sell_amount / 100 }}</td>
+          <td>{{ trade.buy_currency }}</td>
+          <td>{{ trade.buy_amount / 100 }}</td>
+          <td>{{ trade.rate }}</td>
+          <td>{{ trade.created_at }}</td>
+        </tr>
+      </tbody>
+    </template>
+
+    <template v-else>
+      <!-- show message if no trades yet -->
+      <p>No trades booked yet</p>
+    </template>
+  </div>
 </template>
 
 
 <script>
-    export default {
-        name: 'trade-table',
+export default {
+  name: "trade-table",
 
-        methods: {
-            async getTrades() {
-                try {
-                    const response = await fetch('http://localhost:8000/api/v1/trades')
-                    const data = await response.json()
-                    this.trades = data
-                } catch (error) {
-                    // TODO: provide user with feedback
-                    console.error(error)
-                }
-            }
-        },
-
-        data() {
-            return {
-                trades: []
-            }
-        },
-
-        mounted() {
-            this.getTrades()
-        },
-
-        computed: {
-            hasTrades: function() {
-                return this.trades.length
-            }
-        }
+  methods: {
+    async getTrades() {
+      try {
+        const response = await fetch("http://localhost:8000/api/v1/trades");
+        const data = await response.json();
+        this.trades = data;
+      } catch (error) {
+        // TODO: provide user with feedback
+        console.error(error);
+      }
     }
+  },
+
+  data() {
+    return {
+      trades: []
+    };
+  },
+
+  mounted() {
+    this.getTrades();
+  },
+
+  computed: {
+    hasTrades: function() {
+      return this.trades.length;
+    }
+  }
+};
 </script>
 
 
-<style scoped></style>
+<style scoped>
+</style>
